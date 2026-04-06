d27e11b3c3e364117e1fe50c3651617ec44cd3c0
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-06 19:42:47 -0300

    Update Differential_Expression_Analysis.R

--- code/Differential_Expression_Analysis.R
+++ code/Differential_Expression_Analysis.R
@@ -1,8 +1,9 @@
+########################################################################
+# First phase, Tissue.Type
 # Create DESeqDataSet from your prepared matrix
 dds <- DESeqDataSetFromMatrix(countData = read_counts_table,
                               colData = sample_sheet_data,
                               design = ~ Tissue.Type)
-
 # Run DeSeq2
 dds <- DESeq(dds)
 
@@ -14,3 +15,18 @@ res_tumor_normal<-data.frame(res_tumor_normal[which(res_tumor_normal$padj<0.05 &
 
 # Write a data frame to Excel
 write_xlsx(res_tumor_normal,  paste(project_folder,"Supplemental_Table_S1.xlsx",sep="" ))
+########################################################################
+# Second phase, Tissue.Type
+# First phase, Tissue.Type
+# Create DESeqDataSet from your prepared matrix
+dds <- DESeqDataSetFromMatrix(countData = read_counts_table,
+                              colData = sample_sheet_data,
+                              design = ~ Tumor.Descriptor)
+
+# Run DeSeq2
+dds <- DESeq(dds)
+
+# Obtain the results
+res_Primary_normal <- results(dds, contrast=c("Tissue.Type","Primary","Normal"))
+res_Primary_normal <- results(dds, contrast=c("Tissue.Type","Primary","Normal"))
+
