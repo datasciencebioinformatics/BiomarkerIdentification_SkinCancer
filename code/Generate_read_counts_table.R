beaeca5d8bebc0250f3abe577cff005797cbee4d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-06 07:54:27 -0300

    Update Generate_read_counts_table.R

--- code/Generate_read_counts_table.R
+++ code/Generate_read_counts_table.R
@@ -1,39 +1,17 @@
 ###################################################################################
 # Set the project configuration
 # Set the path to manifest file
-Manifest_file=paste(project_folder,"metadata/dc_manifest.2026-04-02.131024.txt",sep="")
-
-# Set the path to metadata cohort file
-Metadata_file=paste(project_folder,"metadata/metadata.cohort.2026-04-01.json",sep="")
+Manifest_file=paste(project_folder,"metadata/gdc_manifest.2026-04-05.155802.txt",sep="")
 
 # Set the path to Sample sheet file
-Sample_sheet_file=paste(project_folder,"metadata/gdc_sample_sheet.2026-04-01.tsv",sep="")
+Sample_sheet_file=paste(project_folder,"metadata/gdc_sample_sheet.2026-04-05.tsv",sep="")
 
 # Set the path to Sample sheet filtered file
 Sample_sheet_filtered_file=paste(project_folder,"metadata/gdc_sample_sheet_star_gene_counts.tsv",sep="")
 
-# Set the path to biospecimen cohort file
-Biospecimen_file=paste(project_folder,"metadata/biospecimen.cohort.2026-04-01.tar.gz",sep="")
-
-# Set the path to clinical cohort file
-Clinical_file=paste(project_folder,"metadata/clinical.cohort.2026-04-01.tar.gz",sep="")
-
-# Set the path to clinical file
-clinical_file=paste(project_folder,"metadata/clinical.tsv",sep="")
-
-# Set the path to sample file
-sample_file=paste(project_folder,"metadata/sample.tsv",sep="")
-
 # Take the list of gene ids
 gene_ids_file<-paste(project_folder,"metadata/gene_ids.txt",sep="")
 ###################################################################################
-# Load data
-# Load clinical data
-clinical_data<-read.delim(file = clinical_file, sep = '\t', header = TRUE,fill=TRUE)
-
-# Load sample data
-sample_data<-read.delim(file = sample_file, sep = '\t', header = TRUE,fill=TRUE)
-
 # Load sample sheet data
 sample_sheet_data<-read.delim(file = Sample_sheet_filtered_file, sep = '\t', header = TRUE,fill=TRUE)
 
